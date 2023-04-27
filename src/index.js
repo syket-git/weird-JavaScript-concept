ed7266b72d44d7b8673beadfeec2ba692342b308
Syket Bhattachergee <39830305+syket-git@users.noreply.github.com>
2023-04-27 18:15:08 +0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const calc = "2" + "2" - "2"; // =>  20
+
+const check = 0.2 + 0.1 === 0.3; // false
+
+const check2 = undefined - false; // NaN
+
+console.log(false === 0); // false
+
+console.log(false == 0); // true
+
+console.log("5" + 1); // '51'
+
+console.log("51" - 1); // 50
+
+console.log(parseInt(Infinity, 10));
+
+console.log(parseInt(Infinity, 35));
+
+console.log([] == ![]); // true
+
+console.log([] === ![]); // false
+
+console.log([, , ,].length); // 3
+
+console.log("I love" + +"Icecream"); // I loveNaN
+
+console.log([1, 2, 3] + [4, 5, 6]); // 1,2,34,5,6
+
+console.log(true + true); // 2
+
+console.log(!9 + !9); //0
+
+console.log(NaN + NaN); // NaN
